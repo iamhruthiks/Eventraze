7c1fae2ae3ef3a25c4b92f52400d9a314b82f605
iamhruthiks <iamhruthiks@gmail.com>
2024-08-27 17:50:21 +0530

    create root and auth folders

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Eventraze</h1>
-      <Button variant="destructive">Delete</Button>
+
     </main>
   );
 }
