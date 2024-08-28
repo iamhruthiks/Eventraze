7039ae682234937e95ccc710ae8c459975dc99da
iamhruthiks <iamhruthiks@gmail.com>
2024-08-28 16:22:37 +0530

    install clerk and set up authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,30 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Button } from '../ui/button'
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <header className='w-full border-b'>
+            <div className='wrapper flex items-center justify-between'>
+                <Link href='/' className='w-36'>
+                    <Image src='/assets/images/logo.png' width={128} height={38} alt='Eventraze logo' />
+                </Link>
+                <div className='flex w-32 justify-end gap-3'>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl='/' />
+                    </SignedIn>
+                    <SignedOut>
+                        <Button asChild className='rounded-full' style={{ backgroundColor: " #00b4d8" }} size='lg'>
+                            <Link href='/sign-in'>
+                                Login
+                            </Link>
+                        </Button>
+                    </SignedOut>
+                </div>
+            </div>
+        </header>
     )
 }
 
