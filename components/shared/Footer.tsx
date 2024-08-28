9fd38d97b5a832b62469db8ee6e1336aa75c549e
iamhruthiks <iamhruthiks@gmail.com>
2024-08-28 19:00:44 +0530

    implement footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer className='border-t'>
+            <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+                <Link href='/'>
+                    <Image
+                        src='/assets/images/logo.png'
+                        alt='logo'
+                        width={128}
+                        height={38}
+                    />
+                </Link>
+                <p>2024 Eventraze. All rights reserverd</p>
+            </div>
+        </footer>
     )
 }
 
